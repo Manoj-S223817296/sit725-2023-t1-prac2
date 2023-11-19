c2d86f83f911b0c9093a573577d3d2bbfc68462e
Manoj-S223817296 <s223817296@deakin.edu.au>
2023-11-19 17:20:50 +1100

    Implemented simple calculator and post method

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 let express = require('express');
+const bodyParser = require('body-parser');
 let app = express();
 let port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.render('index.html');
@@ -13,6 +15,36 @@ app.get('/addTwoNumbers',(req,res)=>{
     let num2 = req.query.number2; // this should return 2
     let sum = parseInt(num1) + parseInt(num2);
     let obj = {statusCode:200, message:'success', data:sum}
+
+    res.json(obj);
+});
+
+app.post('/calculate', (req, res) => {
+    const { number1, number2, calType } = req.body;
+
+    let num1 = parseInt(number1);
+    let num2 = parseInt(number2);
+    let operator = req.body.calType;
+    let result;
+    
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 + num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = 0;
+
+    }
+    let obj = {statusCode:200, message:'success', data:result}
 
     res.json(obj);
 });
